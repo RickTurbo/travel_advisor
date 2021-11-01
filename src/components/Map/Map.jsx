cf6718e5f6e9fdf16a3d2cf491ed19176d2b5a47
kojima rikuto <kojikoji7765@icloud.com>
2021-11-02 00:20:54 +0900

    googlemapの登録

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,8 +1,28 @@
+import { useMediaQuery } from "@material-ui/core";
+import GoogleMapReact from "google-map-react";
 import React from "react";
 
+import useStyles from "./styles";
+
 const Map = () => {
+  const classes = useStyles();
+  const isMobile = useMediaQuery("(min-width:600px)");
+
+  const coordinates = { lat: 0, lng: 0 };
+
   return (
-    <h1>Map</h1>
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: 'AIzaSyDXwUTQf2BtLc2AMNmyqJPhDO9JELqsE2k' }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={""}
+        onChange={""}
+        onChildClick={""}
+      ></GoogleMapReact>
+    </div>
   );
 };
 
