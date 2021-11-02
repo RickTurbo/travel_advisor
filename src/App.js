097bf76c3bb7f4d2fc0c45245be42827cda8395c
kojima rikuto <kojikoji7765@icloud.com>
2021-11-02 18:36:43 +0900

    useEffect[bounds,coordinates]

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
 import { CssBaseline, Grid } from "@material-ui/core";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./components/Header/Header";
 import Map from "./components/Map/Map";
 import List from "./components/List/List";
+import { getPlaceData } from "./api";
 
 const App = () => {
+  const [places, setPlaces] = useState([]);
+
+  const [coordinates, setCoordinates] = useState({ lat: 0, lng: 0 });
+  const [bounds, setBounds] = useState(null);
+
+  
+
+  useEffect(() => {
+    console.log(coordinates, bounds);
+    getPlaceData().then((data) => {
+      console.log(data);
+      setPlaces(data);
+    });
+  }, [bounds, coordinates]);
+
   return (
     <>
       <CssBaseline />
       <Header />
       <Grid container spacing={3} style={{ width: "100%" }}>
         <Grid item xs={12} md={4}>
-      <List />
+          <List />
         </Grid>
         <Grid item xs={12} md={8}>
-    <Map/>
+          <Map
+            setCoordinates={setCoordinates}
+            setBounds={setBounds}
+            coordinates={coordinates}
+          />
         </Grid>
       </Grid>
     </>
---
+++ src/api/index.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+const URL =
+  "https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary";
+
+const options = {
+  params: {
+    bl_latitude: "11.847676",
+    tr_latitude: "12.838442",
+    bl_longitude: "109.095887",
+    tr_longitude: "109.149359",
+  },
+  headers: {
+    "x-rapidapi-key": "a7bc143ab1msh97ffb5449fe72e9p12f4a8jsnb5d08facbb68",
+    "x-rapidapi-host": "travel-advisor.p.rapidapi.com",
+  },
+};
+
+
+
+export const getPlaceData = async () => {
+  try {
+    const {
+      data: { data },
+    } = await axios.get(URL, options);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
